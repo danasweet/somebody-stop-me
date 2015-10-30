9f6bea03b209ec0ab20115634252671d9dac540e
Brian Mock & Linas Mikalcius <pair+brianmock.linas1985@devbootcamp.com>
2015-10-30 12:53:28 -0500

    Rename method for finding bus stop

--- app/controllers/bus_stop_controller.rb
+++ app/controllers/bus_stop_controller.rb
@@ -7,7 +7,7 @@ class BusStopController < ApplicationController
 		address = obj.address
 		bus_num = obj.bus_num
 		response = open("google#{address}").read
-		return BusStop.find_closest_by_coord(response.lon,response.lat,bus_num)
+		return BusStop.find_closest_by_coord_and_bus(response.lon,response.lat,bus_num)
 	end
 	
 end
