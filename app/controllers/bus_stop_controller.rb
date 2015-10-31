c40adfeca6883e27613cb8d339127fc2fafb25ed
brianmock <briannealmock@gmail.com>
2015-10-31 12:39:44 -0500

    Send json object back to client

--- app/controllers/bus_stop_controller.rb
+++ app/controllers/bus_stop_controller.rb
@@ -3,12 +3,15 @@ class BusStopController < ApplicationController
 	skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
 
 	def api
-		p params
-    obj = params[:data]
-    address = obj[0]
-    bus_num = obj[1]
+    obj = params["data"]
+    address = obj.split(',')[0]
+    bus_num = obj.split(',')[1]
 		response = Geokit::Geocoders::MultiGeocoder.do_geocode(address)
-    return BusStop.find_closest_by_coord_and_bus(response.longitude,response.latitude,bus_num)
+    respond_to do |format|
+      format.html { return BusStop.find_closest_by_coord_and_bus(response.longitude,response.latitude,bus_num) }
+      format.json  { render :json => BusStop.find_closest_by_coord_and_bus(response.longitude,response.latitude,bus_num) }
+      format.xml  { }
+    end
 	end
 
 end
