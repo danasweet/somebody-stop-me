06307dc6ae9273a1325d2d6896ed28d19f168e3f
Lucas Rim <lucasrim@Lucas-MacBook-Pro.local>
2015-11-01 13:58:09 -0600

    working alamo-fire

--- app/controllers/bus_stop_controller.rb
+++ app/controllers/bus_stop_controller.rb
@@ -1,14 +1,14 @@
 
 class BusStopController < ApplicationController
-	skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
+	protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 
 	def api
-		p params
-    obj = params[:data]
-    address = obj[0]
-    bus_num = obj[1]
-		response = Geokit::Geocoders::MultiGeocoder.do_geocode(address)
-    return BusStop.find_closest_by_coord_and_bus(response.longitude,response.latitude,bus_num)
-	end
+    obj = params
+    address = obj[:address]
+    bus_num = obj[:busLine]
 
+		response = Geokit::Geocoders::MultiGeocoder.do_geocode(address)
+		bus_stop_json = BusStop.find_closest_by_coord_and_bus(response.longitude,response.latitude,bus_num)
+		render json: bus_stop_json
+	end
 end
