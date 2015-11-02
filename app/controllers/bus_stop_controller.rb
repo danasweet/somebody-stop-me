2a7a52aff0bcf49ee65a92ffa9820a9b68b21f6c
brianmock <briannealmock@gmail.com>
2015-11-02 10:34:20 -0600

    Restore other data handling

--- app/controllers/bus_stop_controller.rb
+++ app/controllers/bus_stop_controller.rb
@@ -1,13 +1,15 @@
 class BusStopController < ApplicationController
 	protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format == 'application/json' }
 
-	def api
-    obj = params
-    address = obj[:address]
-    bus_num = obj[:busLine]
-
-		response = Geokit::Geocoders::MultiGeocoder.do_geocode(address)
-		bus_stop_json = BusStop.find_closest_by_coord_and_bus(response.longitude,response.latitude,bus_num)
-		render json: bus_stop_json
-	end
+  def api
+    obj = params["data"]
+    address = obj.split(',')[0]
+    bus_num = obj.split(',')[1]
+    response = Geokit::Geocoders::MultiGeocoder.do_geocode(address)
+    respond_to do |format|
+      format.html { }
+      format.json  { render :json => BusStop.find_closest_by_coord_and_bus(response.longitude,response.latitude,bus_num) }
+      format.xml  { }
+    end
+  end
 end
